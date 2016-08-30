a7ce35cb3c06148053dea5b9939283df6fdf56b2
folkefd <folkefd@gmail.com>
2016-08-30 15:02:07 +0200

    PUSH

--- Övning 1-6/Program.cs
+++ Övning 1-6/Program.cs
@@ -53,6 +53,45 @@ namespace Övning_1_6
 
             //Övning 3
 
+            Console.WriteLine("Mata in grader i Celsius:");
+            string graderC = Console.ReadLine();
+            double graderF = double.Parse(graderC);
+            Console.WriteLine("Det blir " + (graderF * 1.8 + 32 ) + " grader Farenheit.");
+            Console.WriteLine("Tryck ned valfri tangent för att fortsätta...");
+            Console.ReadKey();
+
+            // Övning 4
+
+            Console.Write("Mata in grader i Celsius: ");
+            string graderC = Console.ReadLine();
+            double graderF = double.Parse(graderC);
+            Console.WriteLine("Det blir " + (graderF * 1.8 + 32) + " grader Farenheit.");
+            Console.WriteLine("Tryck ned valfri tangent för att fortsätta...");
+            Console.ReadKey();
+
+            //Övning 5
+
+            Console.Write("Cirkelns radie: ");
+            string radie = Console.ReadLine();
+            double nyttVärde = double.Parse(radie);
+
+            Console.WriteLine("Area: " + (nyttVärde * nyttVärde * 3.14159265359));
+            Console.WriteLine("Omkrets " + (nyttVärde * 2 * 3.14159265359));
+            Console.WriteLine("Tryck ned valfri tangent för att fortsätta...");
+            Console.ReadKey();
+
+            //Övning 6
+
+            Console.WriteLine("Mata in ett tresiffrigt tal:");
+            string inmatat = Console.ReadLine();
+            int ditTal = int.Parse(inmatat);
+
+            Console.WriteLine("Siffersumman är:" + ((ditTal / 100) + ((ditTal / 10) - (ditTal / 100 * 10)) + ((ditTal) - ((ditTal / 10) * 10))));
+            Console.WriteLine("test" + ditTal / 100);
+            Console.WriteLine("test" + ((ditTal / 10) - (ditTal / 100 * 10)));
+            Console.WriteLine("test" + ((ditTal) - ((ditTal/10)*10)) );
+            Console.ReadKey();
+            */
         }
     }
 }
